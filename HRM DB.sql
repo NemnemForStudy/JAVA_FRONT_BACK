41978102fe9cb1ee28bc63e9f2f97265a3a1e9ed
Nemnem <dlackdtjq123@naver.com>
2023-03-06 14:54:01 +0900

    2023-03-06 DB MAKING

---
+++ HRM DB.sql
@@ -0,0 +1,25 @@
+USE EXAMPLE_DB;
+
+-- Employee(employee_number, position, name, age, gender,
+--          academic_ability, birth, tel_Number, address,
+--          address_detail, join_date, resignation_date,
+--          dept, annual_income, note
+--          )
+
+CREATE Table Employee(
+	employee_number INT AUTO_INCREMENT PRIMARY KEY COMMENT '사번',
+    position VARCHAR(10) NOT NULL COMMENT '직급',
+    name VARCHAR(20) NOT NULL COMMENT '이름',
+    age INT NOT NULL COMMENT '나이',
+    gender VARCHAR(10) NOT NULL COMMENT '성별',
+    academic_ability VARCHAR(20) NOT NULL COMMENT '학력',
+    birth DATE NOT NULL COMMENT '생년월일',
+    tel_number VARCHAR(15) NOT NULL UNIQUE COMMENT '폰번호',
+    address TEXT NOT NULL COMMENT '주소',
+    address_detail TEXT NOT NULL COMMENT '상세주소',
+    join_date DATE NOT NULL COMMENT '입사일',
+    resignation_date DATE COMMENT '퇴사일',
+    dept VARCHAR(45) DEFAULT '미정' COMMENT '부서',
+    annual_income INT NOT NULL COMMENT '연봉',
+    note TEXT COMMENT '비고'
+);
